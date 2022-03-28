357e5e4728a09adb3a36d37773c6a555ef0b5af9
Leandro-Pinho <leandroessencia@gmail.com>
2022-03-28 13:21:48 -0300

    configuring mailcatcher

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :validatable
-  
+  after_create :welcome_mail 
+
+  private 
+
+  def welcome_mail 
+    UserMailer.welcome(self).deliver_now 
+  end
 end
